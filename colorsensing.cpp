b116fdea37c6c7513e5b497e0a486ff3197b4dbc
regelFish <168052544+regelFish@users.noreply.github.com>
2025-10-09 14:31:06 -0400

    Update colorsensing.cpp to include IR and simple stop test

--- colorsensing.cpp
+++ colorsensing.cpp
@@ -1,20 +1,32 @@
 #include <Arduino.h>
+#include <assert.h>
 
+
+#define pin0 10
+#define pin1 9
+#define pin2 5
+#define pin3 6
+#define TRIM1 .935
+#define TRIM2 1
 
 #define LED_R 2
-#define LED_G 4
 #define LED_B 3
-#define LED_IR 4
+#define LED_G 4
+#define LED_IR 7
 
 #define PHOTOTRANSISTOR_Visible A5
-#define PHOTOTRANSISTOR_IR 6
+#define PHOTOTRANSISTOR_IR A4
 
 
 #define COLOR_THRESHOLD 50
 #define distanceCalubrationFactor 1
 void initializeColorSensing();
 char currentColor();
+int currentDistance();
+bool stop();
+int readIRValue(int ledPin, int photoPin);
 int readColorValue(int ledPin, int photoPin);
+void setSpeed(int motor1, int motor2);
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,7 +37,14 @@ void setup() {
 }
 
 void loop() {
-    currentColor();
+  if (stop()) {
+    setSpeed(0, 0);
+    Serial.println("STOP");
+    while (stop());
+  } else {
+    setSpeed(100, 100);
+    Serial.println("GO");
+  }
 }
 
 // put function definitions here:
@@ -47,6 +66,17 @@ int readColorValue(int ledPin, int photoPin){
   digitalWrite(ledPin, LOW);
 
   return value;
+}
+
+int readIRValue(int ledPin, int photoPin){
+  digitalWrite(ledPin, LOW);
+  delay(10);
+  int ambient = analogRead(photoPin);
+  digitalWrite(ledPin, HIGH);
+  delay(10);
+  int value = analogRead(photoPin);
+
+  return value - ambient;
 }
 
 char currentColor() {
@@ -75,9 +105,39 @@ char currentColor() {
 }
 
 int currentDistance() {
-  int irValue = readColorValue(LED_IR, PHOTOTRANSISTOR_IR);
-  return irValue * distanceCalubrationFactor;
+  return readIRValue(LED_IR, PHOTOTRANSISTOR_IR);
 }
 
 
+bool stop() {
+  return (currentDistance() < -20);;
+}
 
+void setSpeed(int motor1, int motor2) {
+  motor1 *= TRIM1;
+  motor2 *= TRIM2;
+  if (abs(motor1) > 255 || abs(motor2) > 255) {
+    Serial.println("ERROR: Max absolute speed value is 255");
+    assert(false);
+  }
+  // Serial.print(motor1);
+  // Serial.print(motor2);
+  if (motor1 > 0) {
+    // Serial.println("m1positive");
+    analogWrite(pin0, motor1);
+    analogWrite(pin1, 0);
+  } else {
+    // Serial.println("m1negative");
+    analogWrite(pin1, -motor1);
+    analogWrite(pin0, 0);
+  }
+  if (motor2 < 0) {
+    // Serial.println("positive");
+    analogWrite(pin2, -motor2);
+    analogWrite(pin3, 0);
+  } else {
+    // Serial.println("negative");
+    analogWrite(pin3, motor2);
+    analogWrite(pin2, 0);
+  }
+}
